296dd1e45f81ad70e78a8efd4901799ad7f96160
Matijas Levang <micolevang@gmail.com>
2025-08-13 15:27:26 +0200

    #6 add simple visualization plugin

---
+++ plugins/simple_visualizer/simple_visualizer/simple_visualizer.py
@@ -0,0 +1,22 @@
+from graph_api import Graph
+import json
+import os
+
+class SimpleVisualizer():
+    #Returns DOM for main visualization window
+    def visualize_graph(self, g: Graph)->str:
+        parsedNodes = []
+        for node in g._vertices.values():
+            parsedNodes.append(node._attributes)
+
+        parsedLinks = []
+        for source, targets in g._edges.items():
+            for target in targets:
+                parsedLinks.append({"source": source, "target": target})
+
+        html = ""
+        template_path = os.path.join(os.path.dirname(__file__))
+        with open(template_path + "/templates/layout.html") as file:
+            html = file.read()
+        
+        return html.replace("NODES", json.dumps(parsedNodes)).replace("LINKS", json.dumps(parsedLinks))
