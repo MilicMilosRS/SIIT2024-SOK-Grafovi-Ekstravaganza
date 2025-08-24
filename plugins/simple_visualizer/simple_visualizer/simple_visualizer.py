a5b78ac4906e29aa12baf251d814608df0f86ad5
Matijas Levang <micolevang@gmail.com>
2025-08-24 21:30:54 +0200

    #6 hotfix simple visualization refactor

--- plugins/simple_visualizer/simple_visualizer/simple_visualizer.py
+++ plugins/simple_visualizer/simple_visualizer/simple_visualizer.py
@@ -7,19 +7,25 @@ class SimpleVisualizer(GraphVisualizer):
     def visualize_graph(self, g: Graph, selected_node: Node)->str:
         parsedNodes = []
         for node in g._vertices.values():
-            parsedNodes.append(node._attributes)
+            parsedNodes.append({"attributes": node._attributes})
 
         parsedLinks = []
         for source, targets in g._edges.items():
             for target in targets:
-                parsedLinks.append({"source": source, "target": target})
+                parsedLinks.append({"source": source, "target": target, "attrs": g._edges[source][target]})
 
         html = ""
         template_path = os.path.join(os.path.dirname(__file__))
         with open(template_path + "/templates/layout.html") as file:
             html = file.read()
         
-        return html.replace("NODES", json.dumps(parsedNodes)).replace("LINKS", json.dumps(parsedLinks))
+        html = html.replace("NODES", json.dumps(parsedNodes))
+        html = html.replace("LINKS", json.dumps(parsedLinks))
+        html = html.replace("IS_DIRECTED", "true" if g._is_directed else "false")
+        html = html.replace("SELECTED_ID", '"' + str(selected_node.get_id()) + '"' if selected_node is not None else "null")
+
+        return html
+
     
     def on_switched_to(self):
         pass
